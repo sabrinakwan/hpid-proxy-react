d17f10cf57dd364571758aca491e65785185ad0a
Sabrina Kwan <sabrina.kwan@hp.com>
2019-12-18 13:54:22 -0800

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './header.css';
+// import { NavLink } from "react-router-dom";
+
+function Header() {
+    return (
+        <div className="toolbar" role="banner">
+            <div className="logo"></div> 
+        </div>
+    );
+}
+export default Header;
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from './components/header';
+import Footer from './components/footer/footer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log("Here are my cookies" + document.cookie);
+const routing = (
+    <Router>
+        <Header />
+        <hr />
+        <Switch>
+            <Route exact path='/' component={App} />
+        </Switch>
+        <Footer />
+    </Router>
+);
+ReactDOM.render(routing, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
